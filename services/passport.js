117cfd5a03b81f6145237cbd1f42583d4b7f98f3
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-04-26 19:03:14 -0500

    google mongoose started

--- services/passport.js
+++ services/passport.js
@@ -1,13 +1,27 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log('accessToken', accessToken);
-  console.log('refreshToken', refreshToken);
-  console.log('profile;', profile);
+
+
+    User.findOne({ googleId: profile.id })
+      .then( (existingUser) => {
+        if(existingUser){
+          //we have a record with this user.
+          console.log("This user Exists hahaha");
+        }
+        else{
+          new User({
+            googleId: profile.id
+          }).save();
+        }
+      })  
 }));
