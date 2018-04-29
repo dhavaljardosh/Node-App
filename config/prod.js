afb9a2107c2eeb0a9a6bb8c953c22a5898806ae3
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-04-28 23:00:31 -0500

    removed dev file

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID:  '845456762911-jtlb4qdffgrf6sdtvurjei5dl8vtkl1t.apps.googleusercontent.com',
-  googleClientSecret:'H8wUdTit4EarMYCoQeCZOxFs',
-  mongoURI:'mongodb://dhavaljardosh:dhavaljardosh@ds135252.mlab.com:35252/emaily-prod',
-  cookieKey: 'lelodaleloda'
+  googleClientID:  process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
 };
