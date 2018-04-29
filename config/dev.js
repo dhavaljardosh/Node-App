1bc432f0cbf5aeedac092e3504327d92a52ea30b
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-04-29 01:43:20 -0500

    commited not required file

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID:  '908218094854-iitnin07f28v3cbh7fbgt8o10059d3gh.apps.googleusercontent.com',
+  googleClientSecret:'2ieXlexZtm-lFfd2LI9TnkRq',
+  mongoURI:'mongodb://dhavaljardosh:dhavaljardosh@ds141514.mlab.com:41514/emaily-dev',
+  cookieKey: 'lelodaleloda'
+};
