000410e9e0af0c31568e7fc860104ef26cfafa0a
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-04-29 21:32:59 -0500

    added debug code

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  googleClientID:  '18666329430-2p5f9ggeauot06ou173j1ugksd7np55d.apps.googleusercontent.com',
+  googleClientID: '18666329430-2p5f9ggeauot06ou173j1ugksd7np55d.apps.googleusercontent.com',
   googleClientSecret:'vJSBHKbjD38o1Ye6rBWb9mbS',
   mongoURI:'mongodb://dhavaljardosh:dhavaljardosh@ds141514.mlab.com:41514/emaily-dev',
   cookieKey: 'lelodaleloda'
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if(process.env.NODE_DEV === "production"){
+  console.log("Production is selected");
   module.exports = require("./prod");
 } else{
+  console.log("Development Selected");
   module.exports = require("./dev");
 }
