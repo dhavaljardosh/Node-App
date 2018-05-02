5beef26189e5abc8c9fb6268743410bfa24d2e64
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-05-02 12:02:02 -0500

    Added Routes

--- services/passport.js
+++ services/passport.js
@@ -5,39 +5,37 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
-passport.serializeUser((user,done) => {
-  done(null,user.id);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser((id,done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    });
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
-console.log(keys.googleClientID+" Client ID and Client Secret " + keys.googleClientSecret);
-
+console.log(keys.googleClientID + " Client ID and Client Secret " + keys.googleClientSecret);
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
-  callbackURL:'/auth/google/callback',
+  callbackURL: '/auth/google/callback',
   proxy: true
-}, (accessToken, refreshToken, profile, done) => {
+},
 
+  async (accessToken, refreshToken, profile, done) => {
 
-    User.findOne({ googleId: profile.id })
-      .then( (existingUser) => {
-        if(existingUser){
-          //we have a record with this user.
-          console.log("This user Exists hahaha");
-        }
-        else{
-          new User({
-            googleId: profile.id
-          }).save()
-          .then(user => done(null,user));
-        }
-      })
-}));
+    const existingUser = await User.findOne({googleId: profile.id})
+
+      if (existingUser) {
+        //we have a record with this user.
+        console.log("CHECKED FOR EXSTING USER");
+        done(null,existingUser);
+      } else {
+        const user = await new User({googleId: profile.id}).save();
+        done(null, user);
+      }
+    }
+  )
+);
