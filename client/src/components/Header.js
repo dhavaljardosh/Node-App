2c0b2700cfefdb4afaaa4c368d3048a8fdb7c427
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-05-05 23:21:39 -0500

    complete sat's work

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+
+
+
+  renderContent(){
+     switch(this.props.auth){
+       case null:
+        return;
+       case false:
+        return <li><a href="/auth/google">Login with Google</a></li>;
+       default:
+        return <li><a href="/api/logout">Log Out</a></li>;
+     }
+  }
+
   render() {
+    var sty = {
+      backgroundColor: '#911f5b'
+    }
+    console.log(this.props);
     return (
       <div>
-      <nav>
+      <nav style={sty}>
         <div className="nav-wrapper">
           <a href="#" className="brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
 
-            <li><a href="/auth/google">Login with Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
@@ -18,4 +37,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps ({auth}){
+  return {auth};
+}
+
+export default connect(mapStateToProps)(Header);
