ad2d65dc80f56bf2abc9b85bad2ab73cf6e36b9c
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-05-06 23:08:56 -0500

    Landing adde

---
+++ client/src/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>Emaily</h1>
+      <p>This is one really awesome platform</p>
+    </div>);
+};
+
+export default Landing;
