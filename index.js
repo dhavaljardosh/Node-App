ae691c27838b93f55ca17a280edc7e8652306a5d
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-05-13 05:09:20 -0500

    push some more logic

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.use(passport.session());
 
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 const PORT = process.env.PORT|| 5000;
 
 app.listen(PORT);
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,11 +1,20 @@
 const keys = require('../config/keys');
-const stripe = require('stripe')(keys.stripeSecretKe);
+const stripe = require('stripe')(keys.stripeSecretKey);
 
 
 console.log("Check this key mofo " +keys.stripeSecretKey);
 module.exports = app => {
-  app.post('/api/stripe', (req,res) => {
-    console.log(req.body);
-    
+  app.post('/api/stripe', async (req,res) => {
+
+    const charge = await stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 Loda',
+      source: req.body.id
+    });
+
+    req.user.credits += 5;
+    const user = await req.user.save();
+    res.send(user);
   });
 };
