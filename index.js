b97a3ad2033039ad7faba9fd7f67aee184da8911
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-05-15 21:24:47 -0500

    some more code

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get('*',(req,res) => {
+    res.sendFile(path.resolve(_dirname,'name',))
+  })
+}
 
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
