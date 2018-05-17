a3916b236a0cc931ac88e8c638ae3875e1d5e4fa
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2018-05-16 22:21:16 -0500

    testing key

--- config/prod.js
+++ config/prod.js
@@ -4,5 +4,5 @@ module.exports = {
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey:process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: 'sk_test_bvi7C4X24c5SgK8b2t4YLZMR'
 };
